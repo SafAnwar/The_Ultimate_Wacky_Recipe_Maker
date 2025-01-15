afc55bfc4ec50777b423c2831517b7bafbdabf09
SafAnwar <140514859+SafAnwar@users.noreply.github.com>
2025-01-15 15:53:24 +0000

    Day 3

---
+++ main.py
@@ -0,0 +1,9 @@
+#Day 3
+foodType = input("Name a type of food: ")
+plant = input("Name a plant: ")
+cookingType = input("Name a method of cooking: ")
+burntFoodDescription = input("Give me a description of burnt food: ")
+householdItem = input("Name a household item: ")
+print()
+print("Tonight's dinner:")
+print("For dinner you should make", cookingType, foodType, "with", burntFoodDescription, plant, "on a plate of", householdItem)
